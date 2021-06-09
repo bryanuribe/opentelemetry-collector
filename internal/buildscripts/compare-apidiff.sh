6c80ec37f605e3a2d43c696523d1db4bf2a57489
bryanuribe <buribe@hmc.edu>
2021-06-09 14:13:22 -0700

    updated script

--- internal/buildscripts/compare-apidiff.sh
+++ internal/buildscripts/compare-apidiff.sh
@@ -5,9 +5,9 @@
 usage() {
   echo "Usage: $0"
   echo
+  echo "-c  Check-incompatibility mode. Script will fail if an incompatible change is found. Default: 'false'"
   echo "-p  Package to generate API state snapshot of. Default: ''"
   echo "-d  directory where prior states will be read from. Default: './internal/data/apidiff'"
-  echo "-c  Check-incompatibility mode. Script will fail if an incompatible change is found. Default: 'false'"
   exit 1
 }
 
@@ -16,16 +16,16 @@ input_dir="./internal/data/apidiff"
 check_only=false
 
 
-while getopts "p:d:" o; do
+while getopts "cp:d:" o; do
     case "${o}" in
+        c) 
+            check_only=true
+            ;;
         p)
             package=$OPTARG
             ;;
         d)
             input_dir=$OPTARG
-            ;;
-        c) 
-            check_only=true
             ;;
         *)
             usage
