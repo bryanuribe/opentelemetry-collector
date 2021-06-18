d13b6c77974b37808b527d6b2f5c251d47ae3646
bryanuribe <buribe@hmc.edu>
2021-06-18 07:05:01 -0700

    added cmoment

--- component/application_info.go
+++ component/application_info.go
@@ -29,7 +29,7 @@ type ApplicationStartInfo struct {
 	// Git hash of the source code.
 	GitHash string
 }
-
+//comment
 //comment 
 //comment 
 // DefaultApplicationStartInfo returns the default ApplicationStartInfo.
